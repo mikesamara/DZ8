201c30cec986fab3531a903e0b9c833fd830556a
Maik <samara.maik@yandex.by>
2023-02-18 14:38:42 +0300

    add Task3

---
+++ Task1/Program.cs
@@ -0,0 +1,52 @@
+﻿System.Console.WriteLine("введите количество строк:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("введите количество столбцов:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] arr = new int[n, m];
+FillArrayRandom(arr);
+PrintArray(arr);
+SortToLower(arr);
+Console.WriteLine();
+System.Console.WriteLine("Упорядочный массив");
+PrintArray(arr);
+
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
